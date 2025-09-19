8d8b33dc82bdb155e81a77583a0425f7eec6a6f5
Kunal Mishra <kunal.mishra@timesinternet.in>
2025-09-19 22:18:52 +0530

    created this project for personal usage

---
+++ src/main/java/com/kunal/Real_Time/News/Aggregator/with/Search/Trending/Application.java
@@ -0,0 +1,13 @@
+package com.kunal.Real_Time.News.Aggregator.with.Search.Trending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kunal/Real_Time/News/Aggregator/with/Search/Trending/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal.Real_Time.News.Aggregator.with.Search.Trending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
